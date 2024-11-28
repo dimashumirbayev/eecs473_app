fa1ccbb3873e3deabd7d419f04d2a5ec31024746
Dimash Umirbayev <dimashumirbayev@mac.attlocal.net>
2024-11-28 00:19:16 -0500

    Add home, recordings, and settings tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,34 +2,49 @@ import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
-  return (
-  <Tabs
-    screenOptions={{
-        tabBarActiveTintColor: "#000000", // Color of the icon itself
-        tabBarActiveBackgroundColor: "#ffd33d", // Color to indicate selected tab
-        headerShadowVisible: false,
-    }}
-  >
+    return (
+    <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: "#000000", // Color of the icon itself
+            tabBarActiveBackgroundColor: "#ffd33d", // Color to indicate selected tab
+            headerShadowVisible: false,
+        }}
+    >
     <Tabs.Screen
-      name="index"
-      options={{
-        headerTitle: "Precision Posture",
-        tabBarIcon: ({ focused, color }) =>
-            <Ionicons
-                name = {"barbell-outline"}
-                size = {30}
-            />,
-      }}/>
+        name="index"
+        options={{
+            href: null,
+        }}/>
     <Tabs.Screen
-      name="about"
-      options={{
-        headerTitle: "About",
-        tabBarIcon: ({ focused, color }) =>
-            <Ionicons
-                name = {focused ? "settings-sharp" : "settings-outline"}
-                size = {26}
-            />,
-      }}/>
-  </Tabs>
-  )
+        name="home"
+        options={{
+            headerTitle: "Home",
+            tabBarIcon: ({ focused, color }) =>
+                <Ionicons
+                    name = {"barbell-outline"}
+                    size = {30}
+                />,
+        }}/>
+    <Tabs.Screen
+        name="recordings"
+        options={{
+            headerTitle: "Recordings",
+            tabBarIcon: ({ focused, color }) =>
+                <Ionicons
+                    name = {focused ? "videocam" : "videocam-outline"}
+                    size = {30}
+                />,
+        }}/>
+    <Tabs.Screen
+        name="settings"
+        options={{
+            headerTitle: "Settings",
+            tabBarIcon: ({ focused, color }) =>
+                <Ionicons
+                    name = {focused ? "settings-sharp" : "settings-outline"}
+                    size = {26}
+                />,
+        }}/>
+    </Tabs>
+    )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Text, View, StyleSheet, } from "react-native";
 import { Link } from "expo-router";
 
-export default function Index() { // Index() is the name of this route
+export default function IndexScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>Index screen</Text>
     </View>
   );
 }
