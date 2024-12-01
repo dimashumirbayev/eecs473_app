e212995b6ecfbb087ba009bcea4fa2cea207c292
Dimash Umirbayev <dimashumirbayev@gmail.com>
2024-11-30 23:45:46 -0500

    Create squat/deadlift button on home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,49 +1,143 @@
-import { Text, View, StyleSheet, Alert, ScrollView } from "react-native";
+import { Text, View, StyleSheet, Alert, ScrollView, TouchableOpacity } from "react-native";
 import { useState, useContext } from 'react';
-import Button from "@/components/Button"
 import { RecordingContext } from "@/components/RecordingManager"
 
 export default function HomeScreen() {
 
     const { startRecording, stopRecording, writeDataLine } = useContext(RecordingContext)
     const [isRecording, setIsRecording] = useState(false);
+    const [mode, setMode] = useState("Squat")
 
     return (
-        <View style={styles.container}>
-            <View>
-                <Button
-                    label = {isRecording? "End Recording" : "Start Recording"}
-                    onPress = {() => {
-                        if (isRecording) {
-                            stopRecording("Squat")
-                            alert("Recording Saved")
-                        } else {
-                            startRecording()
-                        }
-                        setIsRecording(!isRecording)
+        <ScrollView style={styles.scrollcontainer}>
+            <View style={styles.modebuttonscontainer}>
+                <TouchableOpacity
+                    style={ (mode=="Squat")? styles.squatbuttonselected : styles.squatbuttonunselected}
+                    onPress={() => {
+                        setMode("Squat")
                     }}
-                />
-            </View>
-            <View>
-                <Button
-                    label = {"write some data"}
-                    onPress = {() => {
-                        writeDataLine("hello im some data")
+                >
+                    <Text style={styles.modetext}> Squat </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={ (mode=="Deadlift")? styles.deadliftbuttonselected : styles.deadliftbuttonunselected}
+                    onPress={() => {
+                        setMode("Deadlift")
                     }}
-                />
+                >
+                    <Text style={styles.modetext}> Deadlift </Text>
+                </TouchableOpacity>
             </View>
-        </View>
+
+            <TouchableOpacity
+                style={styles.recordbutton}
+                onPress={() => {
+                    if (isRecording) {
+                        stopRecording(mode)
+                        Alert.alert(
+                            "Recording Saved", '',
+                            [{
+                                    text: "OK",
+                                    onPress: () => {}
+                                }],
+                            { cancelable: false }
+                        );
+                    } else {
+                        startRecording()
+                    }
+                    setIsRecording(!isRecording)
+                }}
+            >
+                <Text style={styles.recordtext}> { isRecording ? "Stop Recording" : "Start Recording" } </Text>
+            </TouchableOpacity>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
+    scrollcontainer: {
+        backgroundColor: '#25292e',
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
+    modebuttonscontainer: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 15,
+        height: 68,
+        backgroundColor: 'gray',
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+    squatbuttonselected: {
+        left: 10,
+        backgroundColor: '#93d0db',
+        height: '80%',
+        borderRadius: 15,
+        marginRight: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '46%',
+    },
+    squatbuttonunselected: {
+        left: 10,
+        backgroundColor: 'gray',
+        height: '80%',
+        borderRadius: 15,
+        marginRight: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '46%',
+    },
+    deadliftbuttonselected: {
+        position: 'relative',
+        right: -13,
+        backgroundColor: '#93d0db',
+        height: '80%',
+        borderRadius: 15,
+        marginLeft: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '46%',
+    },
+    deadliftbuttonunselected: {
+        position: 'relative',
+        right: -13,
+        backgroundColor: 'gray',
+        height: '80%',
+        borderRadius: 15,
+        marginLeft: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '46%',
+    },
+    modetext: {
+        color: 'white',
+        fontSize: 20,
+    },
+    recordbutton: {
+        backgroundColor: 'gray',
+        height: 68,
+        borderRadius: 20,
+        marginTop: 15,
+        marginLeft: 10,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    recordtext: {
+        color: 'white',
+        fontSize: 20,
+    }
 });
